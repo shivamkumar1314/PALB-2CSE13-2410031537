bbc50b981ba78f025ddb9aa7f41b657a510a58dc
shivamkumar1314 <shivam.kumar3.cs28@iilm.edu>
2026-02-22 14:32:42 +0530

    Add files via upload

---
+++ prob 16.py
@@ -0,0 +1,17 @@
+def merge_intervals(intervals):
+    intervals.sort()
+    merged = [intervals[0]]
+
+    for current in intervals[1:]:
+        last = merged[-1]
+
+        if current[0] <= last[1]:
+            last[1] = max(last[1], current[1])
+        else:
+            merged.append(current)
+
+    return merged
+
+
+# Example
+print(merge_intervals([[1, 3], [2, 6], [8, 10], [15, 18]]))
